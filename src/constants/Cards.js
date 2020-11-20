3939766f4d20b266c14c3ee756cb0c5e605869b7
valvic6 <valvic6@192-168-0-103.rdsnet.ro>
2020-11-20 14:50:51 +0200

    implemented the rest of the API methods, edit and put

--- src/constants/Cards.js
+++ src/constants/Cards.js
@@ -1,3 +1,3 @@
-export const BASE_URL = 'http://ba74c23488bc.ngrok.io';
+export const BASE_URL = 'http://5efcc75f6136.ngrok.io';
 export const ERROR_MESSAGE = 'API request error';
 export const LIMIT_API_RESPONSE_ITEMS = 100;
--- src/screens/CardEditScreen.js
+++ src/screens/CardEditScreen.js
@@ -2,23 +2,36 @@ import React, { useContext } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Context } from '../context/CardsContext';
 import CardForm from '../components/CardForm';
+import cardsApi from '../api/cardsApi';
 
 const CardEditScreen = ({ navigation }) => {
   const personId = navigation.getParam('personId');
   const { state, dispatch } = useContext(Context);
   const currentPerson = state.find(person => person.id === personId);
   
-  const editPerson = (person, navigateCallback) => {
-    dispatch({type: 'edit_person', payload: {
-      personId: currentPerson.id.toString(), 
-      firstName: person.firstName, 
-      lastName: person.lastName, 
-      age: person.age,
-      color: person.color
-    }});
-    if(navigateCallback) {
-      navigateCallback();
-    }
+  const editPerson = async (person, navigateCallback) => {
+    try {
+      await cardsApi.put(`/cards/${currentPerson.id}`, {
+        firstName: person.firstName, 
+        lastName: person.lastName, 
+        age: person.age,
+        color: person.color
+      });
+
+      dispatch({type: 'edit_person', payload: {
+        personId: currentPerson.id.toString(), 
+        firstName: person.firstName, 
+        lastName: person.lastName, 
+        age: person.age,
+        color: person.color
+      }});
+      
+      if(navigateCallback) {
+        navigateCallback();
+      };
+    } catch(error) {
+      console.error(error);
+    };
   };
 
   return (
